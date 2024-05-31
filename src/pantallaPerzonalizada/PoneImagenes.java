54ed6788c6a996f5a9e348cff916dbe2d40e2eff
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-05-31 00:15:42 -0600

    -Cambios en PoneImagenes.java *Se añaden las librerias necesarias líneas 3-5 *Se crean los atributos de la clase líneas 16-18 *Se añade el constructor de la clase para establer los valores a los atributos líneas 26-31 *Se añade el código para crear una imagen y luego dibujarla en los graficos líneas 37-40

--- src/pantallaPerzonalizada/PoneImagenes.java
+++ src/pantallaPerzonalizada/PoneImagenes.java
@@ -5,35 +5,37 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 /**
- * Esta clase se encarga del manejo de imagenes en nuestro programa utilizando la clase Graphics
+ * Esta clase se encarga del manejo de imagenes en un panel de nuestro programa utilizando la clase 
  * @author Emmanuelito18
  * @param ancho Número de tipo entero mayor o igual a 0, determina el ancho de la imagen
  * @param alto Número de tipo entero mayor o igual a 0, determina el lato de la imagen
  * @param ruta String que indica en donde se guarda la imagen a poner
  */
 public class PoneImagenes extends javax.swing.JLabel{
-    private int ancho;//width
-    private int alto;//height
-    private String ruta;//path
+    // <editor-fold defaultstate="collapsed" desc="Atributos de la clase">
+    private int ancho;//width del JPanel
+    private int alto;//height del JPanel
+    private String ruta;//path de la imagen a utilizar
+    // </editor-fold>
 
     /**
-     * 
+     * El contructor se utiliza para establecer los datos a los atributos de la clase
      * @param jpanel
      * @param ruta 
      */
-    public PoneImagenes(JPanel jpanel,String ruta){
-        this.ruta=ruta;
-        this.ancho=jpanel.getWidth();
-        this.alto=jpanel.getHeight();
-        this.setSize(ancho,alto);
+    public PoneImagenes(JPanel jpanel,String ruta){//constructor de la clase
+        this.ruta=ruta;//establece la ruta de la imagen a utilizar
+        this.ancho=jpanel.getWidth();//obtiene el ancho del JPanel
+        this.alto=jpanel.getHeight();//obtiene el alto de Jpanel
+        this.setSize(ancho,alto);//establece el tamaño del objeto PoneImagenes 
     }
     
     /**
-     * 
+     * Se utiliza para dibujar la imagen en el componente gráfico
      * @param graphics 
      */
     public void paint(Graphics graphics){
-        ImageIcon imagen=new ImageIcon(getClass().getResource(ruta));
-        graphics.drawImage(imagen.getImage(),0,0,ancho,alto,null);
+        ImageIcon imagen=new ImageIcon(getClass().getResource(ruta));//crea un objeto ImageIcon con la ruta especificada
+        graphics.drawImage(imagen.getImage(),0,0,ancho,alto,null);//dibuja la imagen en las coordenadas 0,0 con el alto y ancho especificados
     }
 }
