471ce41515c2aa085511c4e5d25f37d17ad4f299
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-05-30 23:06:04 -0600

    -Cambios en Main.java *Se añadió el código para invocar al JFrame principal líneas 14-15

--- src/pantallaPerzonalizada/Main.java
+++ src/pantallaPerzonalizada/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pantallaPerzonalizada;
 
 /**
@@ -15,6 +11,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Principal principal=new Principal();
+        principal.setVisible(true);
     }
     
 }
