6c8757aace9c5db38c4fd258579bb9078b609874
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-05-31 03:10:55 -0600

    -Cambios en Main.java *Se añadio una función lamda que controla la ventana de carga y de bienvenida lineas 16-29 *Se crea hilo para controlar la ejecución de la función lambda (su tiempo de ejecución) línea 21 *Se inicia hilo creado línea 32 **En funcion lambda de tipo Runnable +Se movió el código para crear y mostrar la ventana splash líneas 17-18 +Se añadio código para crear y mostrar la ventana de bienvenida líneas 27-28 +Se añadio código para dormir el hilo creado líneas 20-29

--- src/pantallaPerzonalizada/Main.java
+++ src/pantallaPerzonalizada/Main.java
@@ -1,7 +1,10 @@
 package pantallaPerzonalizada;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- *
+ *Esta clase se encarga de controlar todo lo relacionado al programa principal
  * @author Emmanuelito18
  */
 public class Main {
@@ -10,9 +13,23 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Principal principal=new Principal();
-        principal.setVisible(true);
+        Runnable run=() -> {//crea un objeto de tipo Runnable utilizando una expresion lamda
+            Splash splash=new Splash();//crea un objeto de la clase Splash llamada splash
+            splash.setVisible(true);//hace visible el objeto splash
+            
+            try {
+                Thread.sleep(5000);//hace que el hilo espere 5 segundos
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            splash.dispose();//cierra el objeto de la ventana splash
+            
+            FrmBienvenida bienvenida=new FrmBienvenida();//crea un objeto de la clase FrmBienvenida llamado bienvenida
+            bienvenida.setVisible(true);//hace visible el objeto bienvenida
+        };
+        
+        Thread hiloSplash=new Thread(run);//crea un objeto Thread llamado hiloSplash y le pasa como parametro un objeto de tipo Runnable
+        hiloSplash.start();//inicial el hilo creado
     }
-    
+    //la funcion lambda se utiliza para definir el comportamiento del hilo
 }
